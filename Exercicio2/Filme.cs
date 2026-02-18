5bd3c45f9eed3e7b0e59c4935eb26bbacc13bfcd
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-18 15:23:04 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Filme.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Exercicio2
+{
+    public class Filme
+    {
+        public Filme(string titulo, int duracao, List<string> elenco)
+        {
+            Titulo = titulo;
+            Duracao = duracao;
+            Elenco = elenco;
+        }
+        public string Titulo { get; set; }
+        public int Duracao { get; set; }
+        public List<string> Elenco { get; set; }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"O filme: {Titulo} possui duração de: {Duracao} min e tem como elenco: {string.Join(", ", Elenco)}");
+        }
+        public static void ListarFilmes(List<Filme> filmes)
+        {
+            foreach (var f in filmes)
+                Console.WriteLine($"{f.Titulo} - {f.Duracao} min");
+        }
+
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercicio2;
+using System.Reflection.Metadata;
+
+Filme django = new("Django Livre", 165, new List<string>() { "Jamie Foxx", "Cristoph Waltz" });
+Filme killbill = new("Kill Bill - Vol.1", 111, new List<string>() { "Uma Thurman", "Lucy Liu" });
+Filme pianista = new("O Pianista", 150, new List<string>() { "Adrien Brody", "Cristoph Waltz" });
+Filme cidade = new("Cidade de Deus", 130, new List<string>() { "Alexandre Rodrigues", "Leandro Firmino" });
+Filme tropa = new("Tropa de Elite", 115, new List<string>() { "Wagner Moura", "André Ramiro" });
+
+var minhaLista = new List<Filme> { django, killbill, pianista, cidade, tropa };
+
+Filme.ListarFilmes(minhaLista);
